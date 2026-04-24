25f09a9380ca55c918a71f2fd40eea146bb53863
Garkatron <masitacarg@gmail.com>
2026-04-24 10:24:07 +0100

    infra: local tests working again

---
+++ documentation/testing.md
@@ -0,0 +1,15 @@
+# Testing
+
+```sh
+just up
+just test
+just down
+```
+
+```sh
+just nuke
+just up
+# config medplum
+NODE_ENV=test node ace migration:run
+just test
+```
--- infra/docker/init.sql
+++ infra/docker/init.sql
@@ -1,6 +1,7 @@
 CREATE DATABASE app_test;
-
 CREATE USER app WITH PASSWORD 'secret';
+
+\c app_test
 
 GRANT ALL ON SCHEMA public TO app;
 ALTER SCHEMA public OWNER TO app;
